a2008397bc0a76c65c7052d2082d33830077fe74
soumyadipjccn <mailsoumyadipghosh@gmail.com>
2026-08-09 05:42:50 +0000

    some small change

--- src/tools/tools.py
+++ src/tools/tools.py
@@ -17,7 +17,7 @@ tavily = TavilyClient(api_key=os.getenv("TAVILY_API_KEY"))
 @tool
 def web_search(query : str) -> str:
     """Search the web for recent and reliable information on a topic . Returns Titles , URLs and snippets."""
-    results = tavily.search(query=query,max_results=5)
+    results = tavily.search(query=query,max_results=7)
 
     out = []
 
